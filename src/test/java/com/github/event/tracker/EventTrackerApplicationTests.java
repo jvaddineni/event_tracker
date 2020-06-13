80127b038487f911ba19b976e58e0c10a61f6de8
jvaddineni <41030359+jvaddineni@users.noreply.github.com>
2020-06-13 20:47:56 +0530

    initial project setup

---
+++ src/test/java/com/github/event/tracker/EventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.event.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
